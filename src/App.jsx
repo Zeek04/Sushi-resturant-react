d1332b6d03f87af7f39d8fd88aec44c8047a7760
Ezequiel <cruzezequiel81@gmail.com>
2025-02-23 22:41:04 -0500

    Added Header in the middle of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Landingpage from './components/Landingpage'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Landingpage/>
     </div>
   )
 }
---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../components/Landingpage.css'
+
+const Landingpage = () => {
+  return (
+    <div>
+        <div className="page-header-container">
+            <h1>BONSAI.</h1>
+            <p lang='ja'>世界で最高の寿司</p>
+        </div>
+    </div>
+  )
+}
+
+export default Landingpage
